78c4c827739b31fb2acc03b0138d10acad1cc0f2
arnoson <omasocke@hotmail.de>
2021-05-07 19:45:04 +0200

    Update local libs

--- include/LuaOnArduino.h
+++
@@ -1,6 +0,0 @@
-#ifndef LuaOnArduino_h
-#define LuaOnArduino_h
-
-#include <LuaWrapper.h>
-
-#endif
---
+++ lib/SLIPSerial/SLIPSerial.h
@@ -0,0 +1,38 @@
+#ifndef SLIPSerial_h
+#define SLIPSerial_h
+
+#include <Arduino.h>
+#include <Stream.h>
+#include <usb_serial.h>
+
+/**
+ * The SlipEncodedSerial class from CNMAT's OSC for arduino.
+ * https://github.com/CNMAT/OSC
+ */
+class SLIPSerial: public Stream {
+private:
+	enum erstate { CHAR, FIRSTEOT, SECONDEOT, SLIPESC } rstate;
+  usb_serial_class *serial;
+	
+public:
+	SLIPSerial(usb_serial_class&);
+	operator bool() const { return *serial; }
+	
+	int available();
+	int read();
+  int readBytes( uint8_t *buffer, size_t size);
+	int peek();
+	void flush();
+	
+	void begin(unsigned long);
+	void beginPacket();
+	
+	void endPacket();
+	bool endOfPacket();
+	
+	// Overrides the Stream's write function to encode SLIP.
+	size_t write(uint8_t b);
+  size_t write(const uint8_t *buffer, size_t size);
+};
+
+#endif

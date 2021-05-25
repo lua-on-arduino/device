712d295f6d491b802d979a004d47ece79dbb4734
arnoson <omasocke@hotmail.de>
2021-05-25 17:36:25 +0200

    Cleanup

--- delay-pio-test.py
+++
@@ -1,9 +0,0 @@
-Import("env")
-
-def after_upload(source, target, env):
-  if "UNIT_TEST" in env["CPPDEFINES"]:
-    import time
-    # Give the teensy a little time!
-    time.sleep(1)
-
-env.AddPostAction("upload", after_upload)
